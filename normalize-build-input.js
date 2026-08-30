eba5261c5b6a4f3e1010a8691be5c3be813cd540
mdroad3160-cloud <mdroad3160@gmail.com>
2026-08-30 18:18:46 +0900

    Support const let or var declarations in build normalizer

--- normalize-build-input.js
+++ normalize-build-input.js
@@ -4,7 +4,7 @@ const output=process.argv[3];
 if(!input||!output) throw new Error('usage: node normalize-build-input.js input output');
 let s=fs.readFileSync(input,'utf8');
 for(const name of ['RANK','PACK','GRAMMAR']){
-  const re=new RegExp(`const\\s+${name}\\s*=`);
+  const re=new RegExp(`(?:const|let|var)\\s+${name}\\s*=`);
   if(!re.test(s)) throw new Error(`${name} declaration not found`);
   s=s.replace(re,`const ${name}=`);
 }
