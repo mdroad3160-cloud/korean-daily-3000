dd9cfd0e6076bc33eb3816f371f5357dc9aa432c
mdroad3160-cloud <mdroad3160@gmail.com>
2026-08-31 08:13:53 +0900

    Deploy audited Rank 901-1000 v1.4.9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "korean-daily-3000-direct-v1.4.8-cachefix2";
+const CACHE = "korean-daily-3000-direct-v1.4.9-cachefix2";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
