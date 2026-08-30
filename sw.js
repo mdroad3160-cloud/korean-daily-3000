73867c8625d825f24752ef2e7c9354425ef2d001
mdroad3160-cloud <mdroad3160@gmail.com>
2026-08-30 20:09:30 +0900

    Bump PWA cache for Rank 601-700 release

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "korean-daily-3000-direct-v1.3.4";
+const CACHE = "korean-daily-3000-direct-v1.4.0";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
