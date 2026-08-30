899a94e53c4d516b7dae4f44325f37991119b656
mdroad3160-cloud <mdroad3160@gmail.com>
2026-08-30 18:16:26 +0900

    Normalize source declarations for audited Pages build

---
+++ normalize-build-input.js
@@ -0,0 +1,12 @@
+const fs=require('fs');
+const input=process.argv[2];
+const output=process.argv[3];
+if(!input||!output) throw new Error('usage: node normalize-build-input.js input output');
+let s=fs.readFileSync(input,'utf8');
+for(const name of ['RANK','PACK','GRAMMAR']){
+  const re=new RegExp(`const\\s+${name}\\s*=`);
+  if(!re.test(s)) throw new Error(`${name} declaration not found`);
+  s=s.replace(re,`const ${name}=`);
+}
+fs.writeFileSync(output,s);
+console.log('Normalized build source declarations');
