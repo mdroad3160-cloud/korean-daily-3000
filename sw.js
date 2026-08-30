9a8ec10c0682e82fc2a4bfc6888ffd94709e786e
mdroad3160-cloud <mdroad3160@gmail.com>
2026-08-31 04:02:08 +0900

    Bump PWA cache to v1.4.7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "korean-daily-3000-direct-v1.4.6";
+const CACHE = "korean-daily-3000-direct-v1.4.7";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
