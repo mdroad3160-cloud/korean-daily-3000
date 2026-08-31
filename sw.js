a7490a5f5d76edbd1ec01e77da9ba70960252a43
mdroad3160-cloud <mdroad3160@gmail.com>
2026-08-31 11:53:19 +0900

    Bump app cache for v1.4.13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "korean-daily-3000-direct-v1.4.12-cachefix2";
+const CACHE = "korean-daily-3000-direct-v1.4.13-cachefix2";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
