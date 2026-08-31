f06934393a1387dad63e3ffc02b1e926844032f4
mdroad3160-cloud <mdroad3160@gmail.com>
2026-08-31 15:45:49 +0900

    Bump app cache for Rank 1600

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "korean-daily-3000-direct-v1.4.14-cachefix2";
+const CACHE = "korean-daily-3000-direct-v1.4.15-cachefix2";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
