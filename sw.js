dd4ec8dca895e22319db82eb862bc31a6276b308
mdroad3160-cloud <mdroad3160@gmail.com>
2026-09-01 07:36:08 +0900

    Bump service worker cache for Daily 5000 extension [deploy]

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "korean-daily-3000-direct-v1.4.16-cachefix2";
+const CACHE = "korean-daily-3000-direct-v1.5.0-daily5000";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
