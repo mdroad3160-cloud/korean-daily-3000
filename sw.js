9d00edcb1ebf7fc15ae75f3dffe375316ddf7b6e
mdroad3160-cloud <mdroad3160@gmail.com>
2026-09-02 07:40:46 +0900

    [deploy] Daily5000 extend next ranks v1.6.9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "korean-daily-3000-direct-v1.6.8-daily5000";
+const CACHE = "korean-daily-3000-direct-v1.6.9-daily5000";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
