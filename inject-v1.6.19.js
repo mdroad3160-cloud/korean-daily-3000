07209689396d61b26a1a7ed74e08416a8e5623c9
mdroad3160-cloud <mdroad3160@gmail.com>
2026-09-02 17:42:40 +0900

    Add Daily5000 v1.6.19 injector

---
+++ inject-v1.6.19.js
@@ -0,0 +1,9 @@
+'use strict';
+const fs=require('fs');
+const path=require.resolve('./inject-v1.6.16.js');
+let code=fs.readFileSync(path,'utf8');
+code=code.replace("VERSION='1.6.16'","VERSION='1.6.19'")
+  .replace("require('./pack-next-v1.6.16.js')","require('./pack-next-v1.6.19.js')")
+  .replace(/\nrequire\('child_process'\)\.execFileSync\(process\.execPath,\['inject-v1\.6\.17\.js',file\],\{stdio:'inherit'\}\);\s*$/,'\n');
+if(!code.includes("VERSION='1.6.19'")||!code.includes("pack-next-v1.6.19.js")) throw Error('v1.6.19 template rewrite failed');
+new Function('require','process',code)(require,process);
