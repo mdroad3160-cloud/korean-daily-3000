436892be0d300c78a0ce5c870b3e296bba7b4e96
mdroad3160-cloud <mdroad3160@gmail.com>
2026-09-02 16:27:19 +0900

    Bump Daily5000 service worker cache v1.6.18

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "korean-daily-3000-direct-v1.6.17-daily5000";
+const CACHE = "korean-daily-3000-direct-v1.6.18-daily5000";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
